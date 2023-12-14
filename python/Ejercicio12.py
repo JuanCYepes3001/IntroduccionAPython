a56ee6831ab9d7d15e336dd20704846b8f514ece
JuanCYepes3001 <152404472+JuanCYepes3001@users.noreply.github.com>
2023-12-14 16:28:49 -0500

    feat- ejercicio 12

---
+++ python/Ejercicio12.py
@@ -0,0 +1,21 @@
+
+height = float(input("Ingrese su estatuta: "))
+weight = float(input("Ingrese su peso: "))
+age = int(input("Ingrese su edad: "))
+
+imc = (weight)/(height**2)
+
+if age < 45 and imc < 22:
+  print("Tiene un riesgo bajo")
+
+elif age >= 45 and imc < 22:
+  print("Tiene un riesgo medio")
+
+elif age < 45 and imc >= 22:
+  print("Tiene un riesgo medio")
+
+elif age >= 45 and imc >= 22:
+  print("Tiene un riesgo alto")
+
+else:
+  print("No tiene riesgo o ya estas muerto")
